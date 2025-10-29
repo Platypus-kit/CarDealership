ce127665231bf72563f8c5ea7fbb5306731fcc26
Platypus-kit <nikpan06@mail.ru>
2025-10-29 18:39:15 +0300

    Create CarInventoryConfig and DbSet for ValueObjects

---
+++ InventoryService/Config/CarInventoryConfig.cs
@@ -0,0 +1,48 @@
+﻿using InventoryService.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using InventoryService.Domain.Entities;
+
+namespace InventoryService.Config
+{
+    public class CarInventoryConfig : IEntityTypeConfiguration<CarInventory>
+    {
+        public void Configure(EntityTypeBuilder<CarInventory> builder)
+        {
+            builder.ToTable("CarInventory");
+            builder.HasKey(x => x.Id);
+            builder.HasOne(i => i.AdditionalInformation)
+                 .WithOne(c => c.CarInventory)
+                 .HasForeignKey<AdditionalInformation>(t => t.Id)
+                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("Indicator");
+            builder.HasKey(x => x.Id);
+            builder.HasOne(i => i.Indicator)
+                .WithOne(c => c.CarInventory)
+                .HasForeignKey<Indicator>(t => t.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            builder.ToTable("Location");
+            builder.HasKey(x => x.Id);
+            builder.HasOne(i => i.Location)
+                .WithOne(c => c.CarInventory)
+                .HasForeignKey<Location>(t => t.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("Position");
+            builder.HasKey(x => x.Id);
+            builder.HasOne(i => i.Position)
+                .WithOne(c => c.CarInventory)
+                .HasForeignKey<Position>(t => t.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("ZoneTime");
+            builder.HasKey(x => x.Id);
+            builder.HasOne(i => i.ZoneTime)
+                .WithOne(c => c.CarInventory)
+                .HasForeignKey<ZoneTime>(t => t.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- InventoryService/Data/AppDbContext.cs
+++ InventoryService/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using InventoryService.Config;
 using InventoryService.Domain.Entities;
+using InventoryService.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
@@ -8,6 +9,12 @@ namespace InventoryService.Data
     public class AppDbContext : DbContext
     {
         public DbSet<CarInventory> CarInventories { get; set; }
+        public DbSet<AdditionalInformation> AdditionalInformation { get; set; }
+        public DbSet<Indicator> Indicator { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Position> Position { get; set; }
+        public DbSet<ZoneTime> ZoneTime { get; set; }
+
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
